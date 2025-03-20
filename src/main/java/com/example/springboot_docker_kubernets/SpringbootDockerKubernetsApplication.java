46c81746c90fe91090d00320a1ddf54d0e001582
tusharjadhav2386 <tushar.jadhav2386@gmail.com>
2025-03-20 15:53:33 +0530

    new project

---
+++ src/main/java/com/example/springboot_docker_kubernets/SpringbootDockerKubernetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_docker_kubernets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerKubernetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerKubernetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_docker_kubernets/SpringbootDockerKubernetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_docker_kubernets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerKubernetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
